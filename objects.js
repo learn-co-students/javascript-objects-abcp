151d99c60a53fbf7352bfacc18a848a0a2829e4d
Ki Moon <eddiemoonie@gmail.com>
2018-01-14 21:44:21 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,16 @@
+var playlist = {
+  Drake: "Take Care",
+  Rihanna: "Work"
+}
+
+function updatePlaylist (playlist, artist, song) {
+  playlist[artist] = song
+  
+  return playlist
+}
+
+function removeFromPlaylist (playlist, artist) {
+  delete playlist[artist]
+  
+  return playlist
+}
