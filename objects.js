f5e50937dc1208b174a745b71e4a6c999fa5e20c
Jay Chen <jscorpionse@yahoo.com>
2018-01-17 07:11:52 +0000

    Done.

--- objects.js
+++ objects.js
@@ -1,2 +1,13 @@
 
-var playlist = new Object();
+var playlist = new Object({artistName: "songTitle1"});
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist[artistName];
+  return playlist;
+}
