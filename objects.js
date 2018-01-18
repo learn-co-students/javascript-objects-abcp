5dfa4ce4e8290861f778927397786066a6d41ede
MATTHEW SHIELDS <matthew.duncan.shields@gmail.com>
2018-01-18 06:38:00 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {lifehouse: "Storm"};
+
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song;
+}
+
+function removeFromPlaylist (playlist, artist){
+  delete playlist[artist];
+}
